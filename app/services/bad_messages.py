10b2386233224aa54ebdeb2d93f4279aec5af008
arthuravila26 <arthur.la@hotmail.com>
2021-03-09 21:45:41 -0300

    Included more phrases

--- app/services/bad_messages.py
+++ app/services/bad_messages.py
@@ -1,3 +1,7 @@
 bad_messages = ["Yours biggest problem it's you",
                  "Before start, give up!",
-                 "Some people said that was impossible, you went there to prove that was really impossible"]
+                 "Some people said that was impossible, you went there to prove that was really impossible",
+                 "The courage to ignore the consequence of an obviously failure",
+                 "Wasn't easy but I fail again",
+                 "Wake up!! The failure is waiting for you",
+                 "Everything is going to change even it's for get worst."]
--- app/services/good_messages.py
+++ app/services/good_messages.py
@@ -1,3 +1,7 @@
 good_messages = ["You should cleanse your mind of all the bad vibes. Let the good vibes flow instead.",
                  "Remember that you will attract the energy that you are giving off. So spread good vibes, give a lot of love and think positive at all times. Enjoy life!",
-                 "Stay strong despite of the bad things that happened. Make them wonder how you are still smiling despite of everything."]
+                 "Stay strong despite of the bad things that happened. Make them wonder how you are still smiling despite of everything.",
+                 "I don’t care how many likes or follows you have, I care if you show up and share from your soul.",
+                 "Sometimes having fun with your best friend is all the therapy you need.",
+                 "A beautiful day begins with a beautiful mindset.",
+                 "Your inner world creates your outer world."]
